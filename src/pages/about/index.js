413116edb9d48b1f18dd621fbe25100ed465bafb
Bui Manh <buimanh17112001@gmail.com>
2021-05-31 13:20:47 +0700

    manh

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -3,8 +3,47 @@ import AboutValue from "./coreValue"
 import AboutMission from "./mission"
 import Quote from "./quote"
 import AboutVision from "./vision"
-
+import { useEffect } from 'react'
+import { WOW } from 'wowjs'
 const About = () => {
+  useEffect(() => {
+
+    const aboutBannerWow = new WOW({
+      boxClass: 'about-banner',
+      offset: 0,
+      mobile: false,
+      live: true
+    })
+
+    aboutBannerWow.init()
+
+    const aboutVisionWow = new WOW({
+      boxClass: 'about-vision',
+      offset: 200,
+      mobile: false,
+      live: true
+    })
+
+    aboutVisionWow.init()
+
+    const aboutMissionWow = new WOW({
+      boxClass: 'about-mission',
+      offset: 300,
+      mobile: false,
+      live: true
+    })
+
+    aboutMissionWow.init()
+
+    const aboutValuenWow = new WOW({
+      boxClass: 'about-core-value',
+      offset: 400,
+      mobile: false,
+      live: true
+    })
+
+    aboutValuenWow.init()
+  }, [])
   return (
     <div id='about'>
       <AboutBanner />
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,8 +2,47 @@ import HomeAbout from "./About"
 import HomeBanner1 from "./Banner1"
 import Partials from "./Partials"
 import Reviews from "./Reviewer"
+import { WOW } from 'wowjs'
+import { useEffect } from 'react'
 
 const Home = () => {
+  useEffect(() => {
+    const bannerWow = new WOW({
+      boxClass: 'banner-1',
+      offset: 0,
+      mobile: false,
+      live: true
+    })
+
+    bannerWow.init()
+
+    const aboutWow = new WOW({
+      boxClass: 'about',
+      offset: 150,
+      mobile: false,
+      live: true
+    })
+
+    aboutWow.init()
+
+    const partialsWow = new WOW({
+      boxClass: 'partial',
+      offset: 300,
+      mobile: false,
+      live: true
+    })
+
+    partialsWow.init()
+
+    const reviewsWow = new WOW({
+      boxClass: 'reviews',
+      offset: 350,
+      mobile: false,
+      live: true
+    })
+
+    reviewsWow.init()
+  }, [])
   return (
     <div id='home'>
       <HomeBanner1 />
