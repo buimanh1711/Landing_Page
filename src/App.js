b3ff81ecfead4f9fc38c2fece145bddc1f7a3a3e
Bui Manh <buimanh17112001@gmail.com>
2021-05-31 16:57:14 +0700

    init dev

--- src/App.js
+++ src/App.js
@@ -1,51 +1,51 @@
-// import { useEffect, useState } from 'react'
-// import { Switch, Route } from 'react-router-dom'
-// import { WOW } from 'wowjs'
-// import { Link, useLocation } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { Switch, Route } from 'react-router-dom'
+import { WOW } from 'wowjs'
+import { Link, useLocation } from 'react-router-dom'
 
-// import './static/styles/common.css'
-// import './static/styles/global.scss'
-// import './static/styles/main_layout.scss'
-// import './static/styles/about.scss'
-// import './static/styles/home.scss'
+import './static/styles/common.css'
+import './static/styles/global.scss'
+import './static/styles/main_layout.scss'
+import './static/styles/about.scss'
+import './static/styles/home.scss'
 
-// import './static/styles/responsive.scss'
+import './static/styles/responsive.scss'
 
-// import MainLayout from './layouts/main'
-// import Home from './pages/home'
-// import About from './pages/about'
+import MainLayout from './layouts/main'
+import Home from './pages/home'
+import About from './pages/about'
 
-// function App() {
-//   const location = useLocation()
-//   const asPath = location.pathname || '/'
+function App() {
+  const location = useLocation()
+  const asPath = location.pathname || '/'
 
-//   const scrollTop = () => {
-//     window.scrollTo({
-//       top: 0,
-//       behavior: "smooth"
-//     })
-//   }
+  const scrollTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    })
+  }
 
-//   useEffect(() => {
-//     scrollTop()
-//   }, [asPath])
+  useEffect(() => {
+    scrollTop()
+  }, [asPath])
 
-//   return (
-//     <div className='my-app'>
-//       <Switch>
-//         <Route path='/about'>
-//           <MainLayout>
-//             <About />
-//           </MainLayout>
-//         </Route>
-//         <Route path='/'>
-//           <MainLayout>
-//             <Home />
-//           </MainLayout>
-//         </Route>
-//       </Switch>
-//     </div>
-//   )
-// }
+  return (
+    <div className='my-app'>
+      <Switch>
+        <Route path='/about'>
+          <MainLayout>
+            <About />
+          </MainLayout>
+        </Route>
+        <Route path='/'>
+          <MainLayout>
+            <Home />
+          </MainLayout>
+        </Route>
+      </Switch>
+    </div>
+  )
+}
 
-// export default App
+export default App
